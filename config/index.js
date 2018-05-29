def76856951cd66a57528b706d43ef1bd82ff5cb
Alex Litvinenko <limit85@gmail.com>
2018-05-29 21:09:40 +0300

    Add config and test files, add checks to dangerfile

---
+++ config/index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const path = require('path');
+const _ = require('lodash');
+
+let localConfig;
+
+try {
+  localConfig = require('../local.env');
+} catch (e) {
+  localConfig = {};
+}
+
+//special test env
+if (process.env.NODE_ENV === 'test') {
+  localConfig = {};
+}
+
+// All configurations will extend these options
+// ============================================
+if (!process.env.NODE_ENV) {
+  process.env.NODE_ENV = 'development';
+}
+
+let all = {
+  env: process.env.NODE_ENV,
+  // Root path of server
+  root: path.normalize(__dirname + '/..'),
+
+  // Server port
+  port: process.env.PORT || 9000,
+};
+
+const resultConfig = _.merge(
+  all,
+  require('./' + (process.env.NODE_ENV || 'development') + '.js') || {},
+  localConfig);
+
+
+module.exports = resultConfig;
+
+
--- dangerfile.js
+++ dangerfile.js
@@ -1,9 +1,39 @@
-const {danger, warn} = require('danger')
+const {danger, fail, message, warn} = require('danger');
+const {flatten, intersection, isEmpty, includes} = require('lodash');
 
-  
 // No PR is too small to include a description of why you made a change
 if (danger.github.pr.body.length < 10) {
   warn('Please include a description of your PR changes.');
 }
 
-  
+const modified = danger.git.modified_files;
+const newFiles = danger.git.created_files;
+const deletedFiles = danger.git.deleted_files;
+
+const modifiedConfigFiles = modified.filter(p => includes(p, 'config/'));
+const modifiedTestFiles = modified.filter(p => includes(p, 'test/'));
+
+const touchedFiles = modified.concat(newFiles).concat(deletedFiles);
+const touchedConfigFiles = touchedFiles.filter(p => includes(p, 'config/'));
+
+
+if (touchedConfigFiles.length) {
+  const touchedConfigFilesMD = touchedConfigFiles.join('\n- ');
+  warn('Modified config files in this PR: \n- ' + touchedConfigFilesMD);
+}
+
+if (modified.length) {
+  const modifiedMD = modified.join('\n- ');
+  message('Changed Files in this PR: \n- ' + modifiedMD);
+}
+
+if (newFiles.length) {
+  const newFilesMD = newFiles.join('\n- ');
+  message('New Files in this PR: \n- ' + newFilesMD);
+}
+
+if (deletedFiles.length) {
+  const deletedFilesMD = deletedFiles.join('\n- ');
+  message('Deleted Files in this PR: \n- ' + deletedFilesMD);
+}
+
