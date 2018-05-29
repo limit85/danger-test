084c4d753cae5ede8b6fef9b9907a9864e362346
Alex Litvinenko <limit85@gmail.com>
2018-05-29 21:26:11 +0300

    Add check for issue reference

--- dangerfile.js
+++ dangerfile.js
@@ -1,9 +1,17 @@
 const {danger, fail, message, warn} = require('danger');
 const {flatten, intersection, isEmpty, includes} = require('lodash');
 
+
+const body = danger.github.pr.body;
+
 // No PR is too small to include a description of why you made a change
-if (danger.github.pr.body.length < 10) {
+if (body.length < 10) {
   warn('Please include a description of your PR changes.');
+} else {
+  const r = /#\d+/ig;
+  if (!r.test(body)) {
+    fail('This PR doesn\'t contain reference to issue');
+  }
 }
 
 const modified = danger.git.modified_files;
